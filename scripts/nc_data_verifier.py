2693740b201e7e230beeeed07a1f6c6297dc8b0d
Deepmalya2506 <iamdeepmalya@gmail.com>
2026-04-20 03:45:12 +0530

    Verified the NASA_Copernicus data

--- scripts/nc_data_verifier.py
+++ scripts/nc_data_verifier.py
@@ -1,6 +1,7 @@
 import xarray as xr
 
-file_path=r'copernicus_data\cmems_mod_glo_phy_my_0.083deg_P1D-m_1776613626385.nc'
-ds = xr.open_dataset(file_path)
+filepath=r"copernicus_data\cmems_mod_glo_phy_my_0.083deg_P1D-m_1776613626385.nc"
+ds = xr.open_dataset(filepath)
 print(ds.coords)
 print(ds.variables)
+print(ds.variables.keys())
